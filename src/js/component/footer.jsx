3f138c0645ccf7497adebf23257b8056e6d921a3
dariel0514 <dariel0514@hotmail.com>
2022-09-27 04:49:01 +0000

    First Step

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div id="footer">
+            <span className="position-absolute top-50 start-50 translate-middle">Copyright __ Your Website 2022</span>
+        </div>
+    )
+}
+export default Footer
